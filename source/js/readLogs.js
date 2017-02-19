aa12e390d6fe8baa5ea5d449b2b2d4d6ae9409cc
MarvinMiao <lwmusc2012@gmail.com>
2017-02-18 18:32:38 -0800

    update log views

--- source/js/readLogs.js
+++ source/js/readLogs.js
@@ -1,6 +1,6 @@
 // js functions to dynamically read logs from back-end
 $(document).ready(function monitorTraining() {
-  var atBottom = 1;
+  var atTrainBottom = 1, atDetectBottom = 1;
   var testNum = 0;
   $(".trainingLogRefresh").click(function() {
     for(i = 0; i < 10; ++i)
@@ -20,10 +20,17 @@ $(document).ready(function monitorTraining() {
 
   // check if divs are scrolled to bottom
   $(".train").scroll(function() {
-    if(($(this)[0].scrollTop + $(".train").height()) >= $(this)[0].scrollHeight) {
-      atBottom = 1;
+    if(($(this)[0].scrollTop + $(this).height()) >= $(this)[0].scrollHeight) {
+      atTrainBottom = 1;
     } else {
-      atBottom = 0;
+      atTrainBottom = 0;
+    }
+  })
+  $(".detection").scroll(function() {
+    if(($(this)[0].scrollTop + $(this).height()) >= $(this)[0].scrollHeight) {
+      atDetectBottom = 1;
+    } else {
+      atDetectBottom = 0;
     }
   })
   // this function will repeatedly check the updates of logs
@@ -32,9 +39,12 @@ $(document).ready(function monitorTraining() {
     $("p.trainingLog").append("This is a training output " + testNum +  "<br />")
     $("p.detectionLog").append("This is a detection output " + testNum +  "<br />")
     // jquery function to automatically scroll down to bottom of div
-    if(atBottom === 1) {
+    if(atTrainBottom === 1) {
       $(".train").scrollTop($(".train")[0].scrollHeight)
-    }    $(".detection").scrollTop($(".detection")[0].scrollHeight)
+    }
+    if(atDetectBottom === 1) {
+      $(".detection").scrollTop($(".detection")[0].scrollHeight)
+    }
     //clearInternal(timer)
   }
   timer = setInterval(checker, 500)
