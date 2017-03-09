f8803e90f7f543a2019732682f567a87e4a03c8f
MarvinMiao <lwmusc2012@gmail.com>
2017-03-08 21:38:12 -0800

    fix non photo exits error

--- static/dashboard-angular.js
+++ static/dashboard-angular.js
@@ -106,31 +106,35 @@ app.controller('todoCtrl', function($scope) {
   }
   // delete image and shift to next image
   $scope.removePhoto = function() {
-    var myData = {"hasSofa" : hasSofa, "hasMattress" : hasMattress, 
-                "hasTvMonitor" : hasTvMonitor, "hasFridge" : hasFridge, "hasChair" : hasChair,
-                "imgSrc" : $scope.photos[$scope._Index].src
-                }
-    $.ajax({
-      url: '/imgConfirmation',
-      type: 'POST',
-      contentType: 'application/json',
-      dataType: 'json',
-      data: JSON.stringify(myData),
-      success: function(response) {
-        console.log(response)
-        // convert JSON object into javascript array
-        hasSofa = false;
-        hasMattress = false;
-        hasTvMonitor = false;
-        hasFridge = false;
-        hasChair = false;
-        $scope.photos.splice($scope._Index, 1);
-        $scope._Index = ($scope._Index < $scope.photos.length - 1) ? ++$scope._Index : 0;
-      },
-      error: function(error) {
-        console.log(error)
-      }
-    })
+      // image labels and the image source info
+    console.log($scope._Index)
+    if($scope.photos[$scope._Index]) { // check if any images waitting for confirmation
+      var myData = {"hasSofa" : hasSofa, "hasMattress" : hasMattress, 
+                    "hasTvMonitor" : hasTvMonitor, "hasFridge" : hasFridge, "hasChair" : hasChair,
+                    "imgSrc" : $scope.photos[$scope._Index].src
+                    }
+      $.ajax({
+        url: '/imgConfirmation',
+        type: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify(myData),
+        success: function(response) {
+          console.log(response)
+          // convert JSON object into javascript array
+        },
+        error: function(error) {
+          console.log(error)
+        }
+      })
+      hasSofa = false
+      hasMattress = false
+      hasTvMonitor = false
+      hasFridge = false
+      hasChair = false
+      $scope.photos.splice($scope._Index, 1)
+      $scope._Index = ($scope._Index < $scope.photos.length - 1) ? ++$scope._Index : 0
+    }
   }
   // functions handling object decisions
   $scope.findSofa = function() {
