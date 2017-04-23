c1fb6b5f187b5b85da494560d7878369dd083a7c
Wei-Chung Chen <wcchen101@gmail.com>
2017-04-22 17:52:14 -0700

    add setupdb.py
    1. separate SQLAlcemey db setup file into another filers

---
+++ setupdb.py
@@ -0,0 +1,76 @@
+#testing postgresql with sqlalchemy - test ok
+db = SQLAlchemy(app)
+engine = create_engine('postgresql://localhost/cmpe295')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# #create category instance in the db
+category1 = ImageCategories(category_name='couch')
+category2 = ImageCategories(category_name='mattress')
+category3 = ImageCategories(category_name='tv-monitor')
+category_unknown = ImageCategories(category_name='unknown')
+session.add(category1)
+session.add(category2)
+session.add(category3)
+#
+# create image instance
+image1 = TrainingImageData(category_id='1', path='abc/ccc')
+image2 = TrainingImageData(category_id='2', path='abc/bbb')
+session.add(image1)
+session.add(image2)
+
+#create employee instance
+employee1 = UserData(userName='Andrew', user_password = 'a',workType='admin')
+employee2 = UserData(userName='Eric', user_password = 'b', workType='operator')
+session.add(employee1)
+session.add(employee2)
+
+#creaet unidentified image
+unidentified1 = UnIdentifiedImage(category_id='1', path='aaa/ccc')
+unidentified2 = UnIdentifiedImage(category_id='2', path='bbb/aaa')
+session.add(unidentified1)
+session.add(unidentified2)
+
+# create image input
+imageinput1 = InputImageData(session_id='1', path='abc/a')
+session.add(imageinput1)
+create classification result
+classification1 = ClassificationResult(inputimage_id = '1', category_id='1', batch_id='1')
+classification2 = ClassificationResult(inputimage_id = '3', category_id='9999', batch_id='1')
+session.add(classification1)
+session.add(classification2)
+
+
+try:
+    session.commit()
+except SQLAlchemyError as ex:
+    session.rollback()
+    print(str(ex))
+
+
+for image in session.query(ImageData).all():
+  print("id", image.image_id)
+  print("path", image.path)
+  print("preprocessed", image.preProcessed)
+  print("add date", image.createDate)
+  print("preprcessed date", image.preprocessedDate)
+  print("trained date", image.trainedDate)
+
+for employee in session.query(UserData).all():
+  print("id", employee.user_id)
+  print("name", employee.userName)
+  print("preprocessed", employee.workType)
+  print("add date", employee.createDate)
+
+for imagecategory in session.query(ImageCategories).all():
+  print("id", imagecategory.category_id)
+  print("category name", imagecategory.category_name)
+
+for unidentified in session.query(UnIdentifiedImage).all():
+  print("id", unidentified.unidentified_id)
+  print("unidentified category id", unidentified.category_id)
+  print("unidentified path", unidentified.path)
+  print("unidentified create date", unidentified.createDate)
+
+engine.dispose()
