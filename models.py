e5bb6aaf0df70c401ca966443cd2d6596d104809
Wei-Chung Chen <wcchen101@gmail.com>
2017-04-26 12:20:14 -0700

    update models.py and setupdb.py

--- models.py
+++ models.py
@@ -3,46 +3,48 @@ from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
-# engine = create_engine('postgresql://localhost/cmpe295')
+
 
 #SQL schema
 class ImageCategory(Base):
-	"""
-	RDMS - table for image categories
-	"""
-	__tablename__ = 'imagecategory'
-	category_id = Column(Integer, primary_key = True, autoincrement=True)
-	category_name = Column(String(100), nullable = False)
+    """
+    RDMS - table for image categories
+    """
+    __tablename__ = 'imagecategory'
+    category_id = Column(Integer, primary_key = True, autoincrement=True)
+    category_name = Column(String(100), nullable = False)
 
-	def __init__(self, category_name):
-		self.category_name = category_name
+    def __init__(self, category_name):
+        self.category_name = category_name
 
 
 class AlertImage(Base):
-	"""
-	RDMS - table for input image data
-	"""
-	__tablename__ = 'alertimage'
-	alert_id = Column(Integer, primary_key = True, autoincrement=True)
-	image_name = Column(String(100), nullable=False)
-	alert_datetime = Column(DateTime, nullable = False)
+    """
+    RDMS - table for input image data
+    """
+    __tablename__ = 'alertimage'
+    alert_id = Column(Integer, primary_key = True, autoincrement=True)
+    image_name = Column(String(100), nullable=False)
+    alert_datetime = Column(DateTime, nullable = False)
 
-	def __init__(self, image_name):
-		self.image_name = image_name
-		self.alert_datetime = datetime.utcnow()
+    def __init__(self, image_name):
+        self.image_name = image_name
+        self.alert_datetime = datetime.utcnow()
 
 class ImageConfirmation(Base):
-	"""
-	RDMS - table for classification result
-	"""
+    """
+    RDMS - table for classification result
+    """
 
-	__tablename__ = 'imageconfirmation'
-	confirmation_id = Column(Integer, primary_key = True, autoincrement=True)
-	category_id = Column(Integer, ForeignKey("imagecategory.category_id"), nullable=False)
-	alert_id = Column(Integer, ForeignKey("alertimage.alert_id"), nullable=False)
-	classification_datetime = Column(DateTime, nullable = False)
+    __tablename__ = 'imageconfirmation'
+    confirmation_id = Column(Integer, primary_key = True, autoincrement=True)
+    category_id = Column(Integer, ForeignKey("imagecategory.category_id"), nullable=False)
+    alert_id = Column(Integer, ForeignKey("alertimage.alert_id"), nullable=False)
+    classification_datetime = Column(DateTime, nullable = False)
 
-	def __init__(self, category_id, model):
-		self.category_id = category_id
-		self.classification_datetime = datetime.utcnow()
-		self.model = model
+    def __init__(self, category_id, alert_id):
+        self.category_id = category_id
+        self.classification_datetime = datetime.utcnow()
+        self.alert_id = alert_id
+
+
--- setupdb.py
+++ setupdb.py
@@ -4,7 +4,16 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 import models
-from models import Base, TrainingImageData, UserData, ImageCategories, ClassificationResult, InputImageData
+from models import Base, ImageCategory, ImageConfirmation, AlertImage
+
+#testing postgresql with sqlalchemy - test ok
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+engine = create_engine('postgresql:///illegaldumpingdb')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 #create category instance in the db
 category1 = ImageCategory(category_name='couch')
@@ -14,23 +23,23 @@ category_unknown = ImageCategory(category_name='unknown')
 session.add(category1)
 session.add(category2)
 session.add(category3)
+session.commit()
 
-
-#create employee instance
-alert_image1 = AlertImage(userName='Andrew', user_password = 'a',workType='admin')
-alert_image2 = AlertImage(userName='Eric', user_password = 'b', workType='operator')
-session.add(employee1)
-session.add(employee2)
+#create alert  instance
+alert_image1 = AlertImage(image_name='car1')
+alert_image2 = AlertImage(image_name='car2')
+session.add(alert_image1)
+session.add(alert_image2)
+session.commit()
 
 #create confirmation instance
 confirmation1 = ImageConfirmation(category_id='1', alert_id='1')
-confirmation1 = ImageConfirmation(category_id='2', alert_id='1')
-confirmation1 = ImageConfirmation(category_id='3', alert_id='2')
-
-try:
-    session.commit()
-except SQLAlchemyError as ex:
-    print(str(ex))
+confirmation2 = ImageConfirmation(category_id='2', alert_id='1')
+confirmation3 = ImageConfirmation(category_id='3', alert_id='2')
+session.add(confirmation1)
+session.add(confirmation2)
+session.add(confirmation3)
+session.commit()
 
 
 #   print("add date", employee.createDate)
